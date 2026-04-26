d899f0c1882e3e171a4b0cbda3782a89d8610250
Jamladz <piswap014@gmail.com>
2026-04-26 12:40:50 +0100

    Add files via upload

--- src/pages/doctor/doctor-dashboard.tsx
+++ src/pages/doctor/doctor-dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "../../lib/firebase";
 import {
--- src/pages/doctor/doctor-login.tsx
+++ src/pages/doctor/doctor-login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../lib/firebase";
 import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, signOut } from "firebase/auth";
